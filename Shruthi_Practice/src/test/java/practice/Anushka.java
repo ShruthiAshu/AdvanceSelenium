f23edbd9b0cb865823fbbb1d0bdb528c62f1a6f7
lenovo <lenovo@DESKTOP-ERFC70E>
2025-07-29 17:10:51 +0530

    Anushka and Virat Push

--- Shruthi_Practice/src/test/java/practice/Anushka.java
+++ Shruthi_Practice/src/test/java/practice/Anushka.java
@@ -1,6 +1,6 @@
 package practice;
 
-public class Script1 {
+public class Anushka {
 	public static void main(String[] args) {
 		System.out.println("Hello.......");
 		System.out.println("Hello.......");
